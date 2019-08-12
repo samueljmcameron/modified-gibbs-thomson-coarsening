f21978a02bfdd55261043b6f54ed135ceac0b123
Sam Cameron <samuel.j.m.cameron@gmail.com>
2019-08-12 09:46:00 -0400

    Made a few changes to the experimental bash script that I forgot to save.

--- experiments/2019-08-02/R_avg0_vs_chi0_scans/run_longertime.sh
+++ experiments/2019-08-02/R_avg0_vs_chi0_scans/run_longertime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=10:00:00
 #SBATCH --account=def-arutenbe
-#SBATCH --output=slurmoutput/run_%A_%a.out
+#SBATCH --output=slurmoutput/run_%A.out
 
 module restore standard_modules
 
